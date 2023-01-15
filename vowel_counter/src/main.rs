0134877f37f40eb4b4c0be93fbde2a06116d856a
jolo1993 <jonas.lovig@gmail.com>
2023-01-15 23:24:42 +0100

    vowel and consonant counter completed v0.2 fixed bug with problem with counting whitespce in middle and number

--- vowel_counter/src/main.rs
+++ vowel_counter/src/main.rs
@@ -1,22 +1,25 @@
 use std::io;
 
-fn vowel_counter(sentence: &String) -> i32{
-    let mut result: i32 = 0;
-    let vowels: [char; 5] = ['a','e','i','o','u'];
-    let sentence: String = sentence.trim().to_lowercase();
-    for chars in sentence.chars(){
-        if vowels.contains(&chars){
-            result += 1;
-        }
-    }
-    println!("{}",result);
-    result
+fn sentence_prep(sentence: String) -> String{
+    let words: Vec<&str>= sentence.split_whitespace().collect();
+    let clean_sentence = words.join("");
+    let mut clean_sentence= clean_sentence.to_string();
+    clean_sentence.retain(|c| !c.is_numeric());
+    clean_sentence
 }
 
-
+fn vowel_counter(sentence: &String) -> i32{
+    let mut result_vowels: i32 = 0;
+    let vowels: [char; 5] = ['a','e','i','o','u'];
+    for chars in sentence.chars(){
+        if vowels.contains(&chars){
+            result_vowels += 1;
+        }
+    }
+    result_vowels
+}
 
 fn consonants_counter(sentence: &String) -> i32{
-    let sentence = sentence.trim().to_lowercase();
     let vowel_count: i32 = vowel_counter(&sentence);
     let result: i32 = sentence.len() as i32 - vowel_count;
     result
@@ -25,5 +28,6 @@ fn consonants_counter(sentence: &String) -> i32{
 fn main() {
     let mut sentence = String::new();
     io::stdin().read_line(&mut sentence).expect("need to be a word or sentence");
+    let sentence: String = sentence_prep(sentence);
     println!("number of vowels {} and consonants {}",vowel_counter(&sentence),consonants_counter(&sentence));
 }
