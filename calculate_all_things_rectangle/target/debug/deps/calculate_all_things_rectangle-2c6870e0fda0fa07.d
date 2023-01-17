f5bc6caf3d64b2c7f1f89257b4f8ff51be05ac53
jolo1993 <jonas.lovig@gmail.com>
2023-01-17 23:06:00 +0100

    task 2 nxt lvl

---
+++ calculate_all_things_rectangle/target/debug/deps/calculate_all_things_rectangle-2c6870e0fda0fa07.d
@@ -0,0 +1,5 @@
+C:\Users\Jotl\CLionProjects\Learning_rust\calculate_all_things_rectangle\target\debug\deps\calculate_all_things_rectangle-2c6870e0fda0fa07.rmeta: src\main.rs
+
+C:\Users\Jotl\CLionProjects\Learning_rust\calculate_all_things_rectangle\target\debug\deps\calculate_all_things_rectangle-2c6870e0fda0fa07.d: src\main.rs
+
+src\main.rs:
---
+++ calculate_all_things_rectangle/target/debug/deps/calculate_all_things_rectangle-2d7f157767d321e8.d
@@ -0,0 +1,5 @@
+C:\Users\Jotl\CLionProjects\Learning_rust\calculate_all_things_rectangle\target\debug\deps\calculate_all_things_rectangle-2d7f157767d321e8.rmeta: src\main.rs
+
+C:\Users\Jotl\CLionProjects\Learning_rust\calculate_all_things_rectangle\target\debug\deps\calculate_all_things_rectangle-2d7f157767d321e8.d: src\main.rs
+
+src\main.rs:
